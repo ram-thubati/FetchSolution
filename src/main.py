81e6aae15d1218b42b1b785f4c61afc2f6fe9c16
ramthubati <ram.thubati@outlook.com>
2023-01-05 08:50:57 -0500

    Logging to console and docstring comments

--- src/main.py
+++ src/main.py
@@ -8,23 +8,40 @@ import localstack_client.session as boto3
 class LoginMessage:
 
     def __init__(self,json_message, secret_salt):
+        """
+        Constructor creates an object of type LoginMessage with masked deviceid and ipaddress
+        Parameters:
+            json_message: flattened JSON of message's body
+            secret_salt: a known secret salt to generate the hash for masking deviceid and ipaddress
+        """
         self.user_id = json_message['user_id']
         self.device_type = json_message["device_type"]
+        self.locale = json_message["locale"]
+        
+        #JSON message contain version number in a Major.Minor.Path format
+        #Since the target type is int and not varchar, I chose to store on the
+        #major version number.
+        self.app_version = int(json_message["app_version"].partition('.')[0])
+        
+        #create_date defaults to current system date.
+        self.create_date = datetime.datetime.now().date()
+
+        #masking is achieved thorugh a SHA-256 hash with known SECRET_SALT.
         self.masked_ip = self.calculateHash(json_message["ip"],secret_salt)
         self.masked_device_id = self.calculateHash(json_message["device_id"], secret_salt)
-        self.locale = json_message["locale"]
-        self.app_version = int(json_message["app_version"].partition('.')[0])
-        self.create_date = datetime.datetime.now().date()
 
     def getTuple(self):
         """
-        returns a tuple containing all attributes of this class
+        returns a tuple containing all attributes of class LoginMessage
         """
         return (self.user_id, self.device_type, self.masked_ip, self.masked_device_id, self.locale, self.app_version, self.create_date)
     
     def calculateHash(self,str, secret_salt):
         """
-        Returns a hash of string str
+        Retunrs the SHA 256 hash of string str
+        Paramters:
+            str: The string to hash
+            secret_salt: a known salt. 
         """
         str_with_secret_salt = str + secret_salt
         hash_object = hashlib.sha256(str_with_secret_salt.encode('utf-8'))
@@ -45,6 +62,7 @@ if __name__ == "__main__":
     #infinite loop because a producers can send messages to Q at anytime.
     while(True):
 
+        #tuples created from message batch are appended to this list.
         messages_to_insert = []
 
         # Read a batch of messages from the queue using long polling with wait time of 5 seconds.
@@ -53,6 +71,9 @@ if __name__ == "__main__":
         if "Messages" in response:
             messages = response["Messages"]
 
+            print("Received {} messages from Q at {}".format(len(messages), datetime.datetime.now()))
+
+            #iterate over each message in the reponse and add tuple to list.
             for message in messages:
                 message_body = json.loads(message["Body"])
                 receipt_handle = message["ReceiptHandle"]
@@ -61,7 +82,7 @@ if __name__ == "__main__":
                     m = LoginMessage(message_body,HASH_SECRET_SALT)
                     messages_to_insert.append(m.getTuple())
                 except:
-                    print("unknown message format found. Discarded message.")
+                    print("Unknown message format found. Discarded message.")
                     continue
                 finally:
                     #regardless of the message, it should be deleted from Q once its read.
